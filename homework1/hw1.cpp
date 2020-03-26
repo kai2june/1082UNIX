da3295ae7183f3fbe332a5d4bc1bee3390a43a7c
kai2june <104703027@nccu.edu.tw>
2020-03-26 11:20:33 +0000

    [FEAT] hw1.cpp can print xx points to yy
    yy is correctly printed, which is the actual path

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdio.h>
 #include <sys/types.h>
 #include <dirent.h>
 #include <cctype>
@@ -6,6 +7,8 @@
 #include <vector>
 #include <string>
 #include <sys/stat.h>
+#include <unistd.h>
+#include <cstring>
 #include "hw1.hpp"
 
 bool is_number(const std::string& s)
@@ -27,6 +30,9 @@ int main(int argc, char* argv[])
     DIR* dir;
     struct dirent* ent;
     struct stat sb;
+    struct stat sb_socket;
+    char* linkname;
+    ssize_t r;
 
     if ( (dir = opendir("/proc")) != nullptr )
     {
@@ -53,28 +59,59 @@ int main(int argc, char* argv[])
             {
                 std::string str(*it);
                 str.append("/").append(ent->d_name);
-                std::cout << str << std::endl;
-                if ( stat(str.c_str(), &sb) == -1 )
+
+                // if ( stat(str.c_str(), &sb_socket) == -1 )
+                // {
+                //     perror("stat");
+                //     exit(EXIT_FAILURE);
+                // }
+                if ( lstat(str.c_str(), &sb) == -1 )
                 {
-                    perror("stat");
+                    perror("lstat");
                     exit(EXIT_FAILURE);
                 }
+
+
+
+                // std::cout << sb.st_size << std::endl;
                 // std::cout << "sb : " << sb.st_mode << std::endl << "S_IFMT : " << S_IFMT <<std::endl;
                 // std::cout << "sb.st_mode & S_IFMT : " << (sb.st_mode & S_IFMT) << std::endl;
                 // std::cout << "S_IFLNK : " << S_IFLNK << std::endl;
-                if ( (sb.st_mode & S_IFMT) == S_IFSOCK )
-                    std::cout << "this is a socket." << std::endl;
+                switch (sb.st_mode & S_IFMT) {
+                    case S_IFBLK:  printf("block device\n");            break;
+                    case S_IFCHR:  printf("character device\n");        break;
+                    case S_IFDIR:  printf("directory\n");               break;
+                    case S_IFIFO:  printf("FIFO/pipe\n");               break;
+                    case S_IFLNK:  printf("symlink\n");                 break;
+                    case S_IFREG:  printf("regular file\n");            break;
+                    case S_IFSOCK: printf("socket\n");                  break;
+                    default:       printf("unknown?\n");                break;
+                }
+
+
+                if ( (sb.st_mode & S_IFMT) == S_IFLNK )
+                {
+                    std::cout << "This is a socket: " << str << std::endl;
+                    r = readlink(str.c_str(), linkname, sb.st_size + 100);
+                    if (r < 0)
+                    {
+                        perror("stat");
+                        exit(EXIT_FAILURE);
+                    }
+                    if (r > (sb.st_size+100))
+                    {
+                        fprintf(stderr, "symlink increased in size "
+                        "between lstat() and readlink()\n");
+                        exit(EXIT_FAILURE);
+                    }
+                    linkname[sb.st_size] = '\0';
+                    printf("'%s' points to '%s'\n", str.c_str(), linkname);
+                    // exit(EXIT_SUCCESS);
+                }
             }
         }
         std::cout << std::endl;
     }
-
-
-    // struct stat sb;
-    // for i : b 
-    //     if sb.st_mode & SIFMT == S_IFLNK
-    //          read_link()
-
 
     return 0;
 }
