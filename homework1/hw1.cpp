cfe2597be2582873f95e29b025946402e355392f
kai2june <104703027@nccu.edu.tw>
2020-03-27 18:26:12 +0000

    [FEAT] make some reusable code into functions

--- homework1/hw1.cpp
+++ homework1/hw1.cpp
@@ -16,22 +16,69 @@
 #include <regex.h>
 
 
-bool is_number(const std::string& s)
+enum class CONNECTION
 {
-    return !s.empty() && 
-        std::find_if(
-            s.begin(), 
-            s.end(), 
-            [](unsigned char c) 
-            { 
-                return !std::isdigit(c); 
+    TCP
+  , UDP
+};
+
+
+std::vector<char*> parse_inodes(const CONNECTION& connection_type)
+{
+    std::FILE* fp;
+    std::vector<char*> inodes;
+    if ( connection_type == CONNECTION::TCP )
+    {
+        fp = fopen("/proc/net/tcp", "r");
+    }
+    char buf[1000];
+    if(!fp)
+    {
+        perror("file opening failed");
+        exit(EXIT_FAILURE);
+    }
+    fgets(buf, sizeof buf, fp);
+    while(fgets(buf, sizeof buf, fp) != nullptr)
+    {
+        int cnt = 0;
+        // std::cout << buf << std::endl;
+        char* pch;
+        pch = strtok(buf,  " ");
+        while ( pch != nullptr )
+        {
+            // printf("%zu:\n%s\n", cnt, pch);
+            if ( cnt == 9 )
+            {
+                char* inode = new char[strlen(pch) + 1];
+                memcpy(inode, pch, strlen(pch) + 1);
+                inodes.emplace_back(inode);
             }
-        ) == s.end();
+            pch = strtok(nullptr, " ");
+            ++cnt;
+        }
+    }
+    if(feof(fp))
+    {
+        puts("end of file reached");
+    }
+    return inodes;
 }
 
-std::vector<std::string> search_pid()
+
+bool is_number(const char* s)
 {
-    std::vector<std::string> proc_fd;
+    for(int i=0; i<strlen(s); ++i)
+    {
+        if (!std::isdigit(s[i]))
+            return false;
+    }
+    return true;
+}
+
+
+std::vector<char*> search_pid()
+{
+    std::vector<char*> proc_pid;
     DIR* dir;
     struct dirent* ent;
 
@@ -41,9 +88,9 @@ std::vector<std::string> search_pid()
         {
             if ( is_number(ent->d_name) )
             {
-                std::string str("/proc/");
-                str.append(ent->d_name).append("/fd");
-                proc_fd.emplace_back(str);
+                char* pid = new char[strlen(ent->d_name) + 1];
+                memcpy(pid, ent->d_name, strlen(ent->d_name) + 1);
+                proc_pid.emplace_back(pid);
             }
         }
         closedir(dir);
@@ -51,119 +98,142 @@ std::vector<std::string> search_pid()
         perror("");
         exit(EXIT_FAILURE);
     }
-    return proc_fd;
+    return proc_pid;
 }
 
-bool match_inode(const std::vector<char*>& inodes, int& socket_inode)
+
+void print_filetype(const struct stat& sb)
 {
-    std::string s = std::to_string(socket_inode);
-    // s = s.c_str();
-    for( auto it = inodes.begin(); it!=inodes.end(); ++it )
-    {
-        std::cout << *it << std::endl;
-        if ( strcmp(*it, s.c_str()) == 0 )
-            return true;
+    switch (sb.st_mode & S_IFMT) {
+        // case S_IFBLK:  printf("block device\n");            break;
+        // case S_IFCHR:  printf("character device\n");        break;
+        // case S_IFDIR:  printf("directory\n");               break;
+        // case S_IFIFO:  printf("FIFO/pipe\n");               break;
+        // case S_IFLNK:  printf("symlink\n");                 break;
+        // case S_IFREG:  printf("regular file\n");            break;
+        case S_IFSOCK: printf("socket\n");                  break;
+        // default:       printf("unknown?\n");                break;
     }
-    // std::cout << "current: " << typeid(s.c_str()).name() << std::endl;
-    return false;
 }
+
+
+int search_socketinode(const mode_t& file_mode, const char* path_to_symboliclink)
+{
+    std::cout << "sb.st_size: " << file_mode << std::endl;
+    char* linkname;
+    ssize_t r;
+    if ( (file_mode & S_IFMT) == S_IFLNK )
+    {
+        linkname  = static_cast<char*>(malloc(file_mode + 100));
+        if (linkname == NULL) {
+            fprintf(stderr, "insufficient memory\n");
+            exit(EXIT_FAILURE);
+        }
+
+        r = readlink(path_to_symboliclink, linkname, file_mode + 1);
+        if (r < 0)
+        {
+            perror("stat");
+            exit(EXIT_FAILURE);
+        }
+        if (r > file_mode+1 ) // this line should be if (r>file_mode) {...}, but it'll exit
+        {
+            fprintf(stderr, "symlink increased in size "
+            "between lstat() and readlink()\n");
+            exit(EXIT_FAILURE);
+        }
+        linkname[file_mode] = '\0';
+        char linktype[7];
+        strncpy(linktype, linkname, 6);
+        linktype[strlen(linktype)] = '\0';
+        if ( strcmp(linktype, "socket") == 0)
+        { 
+            printf("'%s' points to '%s'\n", path_to_symboliclink, linkname);
+            int socket_inode = atoi(linkname+8);
+            std::cout << "Yes, it's socket. socket inode : " << socket_inode << std::endl;
+            return socket_inode;
+    //         if ( match_inode(inodes, socket_inode) )
+    //         {
+    //             std::cout << "true" << std::endl;
+    //             // (*it).erase((*it).end() - 3);
+    //             // tcp_pid.emplace_back(*it);
+    //         }
+        }
+    } // if ISLNK
+    return -1;
+}
+
+
+bool is_tcpsocket()
+{
+    
+}
+
+
+// bool match_inode(const std::vector<char*>& inodes, const int& socket_inode)
+// {
+//     char s[20];
+//     int cx = snprintf(s, 20, "%d", socket_inode);
+//     // std::cout << s << std::endl;
+//     for( auto it = inodes.begin(); it!=inodes.end(); ++it )
+//     {
+//         std::cout << *it << std::endl;
+//         if ( strcmp(*it, s) == 0 )
+//             return true;
+//     }
+//     // std::cout << "current: " << typeid(s).name() << std::endl;
+//     return false;
+// }
+
 
 int main(int argc, char* argv[])
 {
-    // crawl /proc/net/tcp
-    std::FILE* fp = fopen("/proc/net/tcp", "r");
-    char buf[1000];
-    std::vector<char*> inodes;
-    if(!fp)
-    {
-        perror("file opening failed");
-        return EXIT_FAILURE;
-    }
-    fgets(buf, sizeof buf, fp);
-    while(fgets(buf, sizeof buf, fp) != nullptr)
-    {
-        std::size_t cnt = 0;
-        // std::cout << buf << std::endl;
-        char* pch;
-        pch = strtok(buf,  " ");
-        while ( pch != nullptr )
-        {
-            // printf("%zu:\n%s\n", cnt, pch);
-            if ( cnt == 9 )
-            {
-                char* tmp = new char[strlen(pch) + 1];
-                memcpy(tmp, pch, strlen(pch) + 1);
-                inodes.emplace_back(tmp);
-            }
-            pch = strtok(nullptr, " ");
-            ++cnt;
-        }
-    }
-    if(feof(fp))
-    {
-        // puts("end of file reached");
-    }
+    //// crawl /proc/net/tcp
+    std::vector<char*> inodes = parse_inodes(CONNECTION::TCP);
     puts("inodes");
     for( std::vector<char*>::iterator it = inodes.begin(); it != inodes.end(); ++it )
         std::cout << *it << std::endl;
 
-    std::vector<std::string> proc_fd = search_pid();
+    //// do something with pid socket
+    std::vector<char*> proc_pid = search_pid();
+    // for( std::vector<char*>::iterator it=proc_pid.begin(); it!=proc_pid.end(); ++it)
+    //     std::cout << *it << std::endl;
+
+    //// search socket_pid
     DIR* dir;
     struct dirent* ent;
     struct stat sb;
-    char* linkname;
-    ssize_t r;
-
-    for( std::vector<std::string>::iterator it = proc_fd.begin(); it!=proc_fd.end(); ++it)
+    std::vector<char*> tcp_pid;
+    for( std::vector<char*>::iterator it = proc_pid.begin(); it!=proc_pid.end(); ++it)
     {
-        if ( (dir = opendir( (*it).c_str() ) ) != nullptr ) 
+        char path_to_pid[300] = "/proc/";
+        strcat(path_to_pid, *it);
+        strcat(path_to_pid, "/fd/");
+        // std::cout << path_to_pid << std::endl;
+        if ( ( dir = opendir( path_to_pid ) ) != nullptr ) 
         {
             while ( (ent = readdir(dir)) != nullptr )
             {
-                std::string str(*it);
-                str.append("/").append(ent->d_name);
+                char* path_to_symboliclink = new char[strlen(path_to_pid) + strlen(ent->d_name) + 1];
+                strcpy(path_to_symboliclink, path_to_pid);
+                strcat(path_to_symboliclink, ent->d_name);
+                std::cout << path_to_symboliclink << std::endl;
 
-                if ( lstat(str.c_str(), &sb) == -1 )
+                if ( lstat(path_to_symboliclink, &sb) == -1 )
                 {
                     perror("lstat");
                     exit(EXIT_FAILURE);
                 }
+                print_filetype(sb);
 
-                if ( (sb.st_mode & S_IFMT) == S_IFLNK )
-                {
-                    r = readlink(str.c_str(), linkname, sb.st_size + 100);
-                    if (r < 0)
-                    {
-                        perror("stat");
-                        exit(EXIT_FAILURE);
-                    }
-                    if (r > (sb.st_size+100))
-                    {
-                        fprintf(stderr, "symlink increased in size "
-                        "between lstat() and readlink()\n");
-                        exit(EXIT_FAILURE);
-                    }
-                    linkname[sb.st_size] = '\0';
-                    if ( linkname[0] == 's' && 
-                         linkname[1] == 'o' &&
-                         linkname[2] == 'c' &&
-                         linkname[3] == 'k' &&
-                         linkname[4] == 'e' &&
-                         linkname[5] == 't'
-                    )
-                    { 
-                        printf("'%s' points to '%s'\n", str.c_str(), linkname);
-                        int socket_inode = atoi(linkname+8);
-                        std::cout << "inode : " << socket_inode << std::endl;
-                        std::cout << match_inode(inodes, socket_inode) << std::endl;
-                    }
-                }
-            }
-        }
+                int socket_inode = search_socketinode(sb.st_mode, path_to_symboliclink);
+                bool tcp_socket;
+                if ( socket > 0 )
+                    tcp_socket = is_tcpsocket();
+            } // while readdir
+        } // if opendir
         std::cout << std::endl;
     }
-
-
 
     return 0;
 }
