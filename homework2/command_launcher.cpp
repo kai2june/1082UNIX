5974fb3af0cdeab902f0ffc2dea844090e031621
kai2june <104703027@nccu.edu.tw>
2020-05-08 09:39:33 +0000

    [FEAT] LD_PRELOAD should to explicitly specified in command line now.
    But this is not what we want, we want only $ ./sandbox chmod
    ./helloworld.cpp 777
    Without $ LD_PRELOAD=./sandbox.so ./sandbox chmod ./helloworld.cpp 777

--- homework2/command_launcher.cpp
+++ homework2/command_launcher.cpp
@@ -10,52 +10,111 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <dirent.h>
+#include <string.h>
 
 #define	FILE_A	"trash.txt"
 #define DIR_A "dirrr"
 // #define	FILE_NULL	"/dev/null"
 
-
 int main(int argc, char* argv[])
 {
+	setenv("LD_PRELOAD", "./sandbox.so", 1);
+	setenv("SO_PATH", "./sandbox.so", 1);
+    setenv("BASE_PATH", ".", 1);
 	if( argc == 1)
+	{
 		std::cout << "no command given" << std::endl; 
-
+		return 0;
+	}
     int opt;
     char optstring[] = "p:d:";
 
 	char* SO_PATH = getenv("SO_PATH");
 	char* BASE_PATH = getenv("BASE_PATH");
 
+	bool minus_optind = false;
     while ((opt = getopt(argc, argv, optstring)) != -1)
     {
+		minus_optind = true;
         if ( opt == 'p' )
+		{
             setenv("SO_PATH", optarg, 1);
-        else if ( opt == 'd' )
+			setenv("LD_PRELOAD", "./sandbox.so", 1);
+			minus_optind = false;
+		}
+		else if ( opt == 'd' )
+		{
             setenv("BASE_PATH", optarg, 1);
+			minus_optind = false;
+		}
 		else
-			std::cout << "usage: ./sandbox [-p sopath] [-d basedir] [--] cmd [cmd args ...]\n-p: set the path to sandbox.so, default = ./sandbox.so\n-d: the base directory that is allowed to access, default = .\n--: seperate the arguments for sandbox and for the executed command\n" << std::endl;
-        // printf("opt = %c\n", opt);
-        // printf("optarg = %s\n", optarg);
-        // printf("optind = %d\n", optind);
-        // printf("argv[optind - 1] = %s\n\n",  argv[optind - 1]);
+		{
+			std::cerr << "usage: ./sandbox [-p sopath] [-d basedir] [--] cmd [cmd args ...]\n-p: set the path to sandbox.so, default = ./sandbox.so\n-d: the base directory that is allowed to access, default = .\n--: seperate the arguments for sandbox and for the executed command\n" << std::endl;
+			exit(-1);
+		}
+		printf("opt = %c\n", opt);
+        printf("optarg = %s\n", optarg);
+        printf("optind = %d\n", optind);
+        printf("argv[optind - 1] = %s\n\n",  argv[optind - 1]);
     }
-	std::cout << SO_PATH << ", " << BASE_PATH << std::endl;
+	if ( minus_optind )
+		--optind;
+	char cmd[100];
+	strcpy(cmd, argv[optind++]);
+	std::cout << "cmd :" << cmd << std::endl;
+	// char* cmd_args[100];
+	// int i = 0;
+	// while ( argv[optind] != NULL )
+	// {
+	// 	cmd_args[i++] = argv[optind++];
+	// }
 
-    // if ( argv[optind] == "--")
-    //     cmd = argv[++optind];
 
-
+	// if ( strcmp(cmd, "chdir") == 0 )
+	// 	chdir(cmd_args[0]);
+	if ( strcmp(cmd, "chmod") == 0 )
+	{
+		std::cout << "This is chmod" << std::endl;
+		int i = 0;
+		const char* pathname = argv[optind++];
+		const char* str_mode = argv[optind++];
+		mode_t mode = 0;
+		mode |= strtol(str_mode, NULL, 8);
+		// execve("chmod", "helloworld.cpp", 777, (char*)NULL, "LD_PRELOAD=./sandbox.so");
+		chmod(pathname, mode);
+	}
+	// if ( strcmp(cmd, "chown") == 0 )
+	// if ( strcmp(cmd, "creat") == 0 )
+	// if ( strcmp(cmd, "fopen") == 0 )
+	// if ( strcmp(cmd, "link") == 0 )
+	// if ( strcmp(cmd, "mkdir") == 0 )
+	// if ( strcmp(cmd, "open") == 0 )
+	// if ( strcmp(cmd, "openat") == 0 )
+	// if ( strcmp(cmd, "opendir") == 0 )
+	// if ( strcmp(cmd, "readlink") == 0 )
+	// if ( strcmp(cmd, "remove") == 0 )
+	// if ( strcmp(cmd, "rename") == 0 )
+	// if ( strcmp(cmd, "rmdir") == 0 )
+	// if ( strcmp(cmd, "stat") == 0 )
+	// if ( strcmp(cmd, "symlink") == 0 )
+	// if ( strcmp(cmd, "unlink") == 0 )
+	// if ( strcmp(cmd, "execl") == 0 )
+	// if ( strcmp(cmd, "execle") == 0 )
+	// if ( strcmp(cmd, "execlp") == 0 )
+	// if ( strcmp(cmd, "execv") == 0 )
+	// if ( strcmp(cmd, "execve") == 0 )
+	// if ( strcmp(cmd, "execvp") == 0 )
+	// if ( strcmp(cmd, "system") == 0 )
 
 	// char *argv[] = { FILE_A, NULL };
 
 	// chdir("~");
-	creat(FILE_A, O_RDWR);
-	chmod("..", 644);
-	chown(FILE_A, 0, 0);
-	// fopen(FILE_A, "r+");
-	link(FILE_A, "link.txt");
-	mkdir(DIR_A, 0755);
+	// creat(FILE_A, O_RDWR);
+	// chmod("..", 644);
+	// chown(FILE_A, 0, 0);
+	// // fopen(FILE_A, "r+");
+	// link(FILE_A, "link.txt");
+	// mkdir(DIR_A, 0755);
 	// open(FILE_A, O_RDONLY);
 	// openat(AT_FDCWD, FILE_A, 0755);
 	// opendir(DIR_A);
@@ -72,8 +131,8 @@ int main(int argc, char* argv[])
 	/// @brief checked
 	// rmdir(DIR_A); 
 	
-	struct stat st;
-	__xstat(3, FILE_A, &st);
+	// struct stat st;
+	// __xstat(3, FILE_A, &st);
 
 	/// @brief checked
 	// symlink(FILE_A, "symlink.txt"); 
@@ -81,7 +140,8 @@ int main(int argc, char* argv[])
 	/// @brief checked
 	// unlink(FILE_A);	
 	
-	// execl(FILE_A, FILE_A, NULL);
+	// int error_code = execl(FILE_A, FILE_A, NULL);
+	// std::cout << "ERROR_CODE: " << error_code << std::endl;
 	// execle(FILE_A, FILE_A, NULL, NULL);
 	// execlp(FILE_A, FILE_A, NULL);
 	// execv(FILE_A, argv);
@@ -91,8 +151,7 @@ int main(int argc, char* argv[])
 	/// @brief checked
 	// system("echo $PATH");
 	
-	// return -1;
-
+	
 	return 0;
 }
 
--- homework2/inject.cpp
+++ homework2/inject.cpp
@@ -63,13 +63,13 @@ int stderr_fd = 2;
 void init() __attribute__((constructor));
 void init()
 {
+    setenv("LD_PRELOAD", "./sandbox.so", 1);
     setenv("SO_PATH", "./sandbox.so", 1);
     setenv("BASE_PATH", ".", 1);
 }
 
 bool is_sub_directory(const char* pathname)
 {
-    std::vector<char*> vec;
     char my_path[200];
     if(pathname[0]!='/' && pathname[0]!='~' && pathname[0]!='.')
     {
@@ -82,17 +82,18 @@ bool is_sub_directory(const char* pathname)
     char* tmp = getenv("PWD");
     char* last_slash = strrchr(tmp, '/');
     char parent[200];
-    std::cout << last_slash - tmp << std::endl;
+    // std::cout << last_slash - tmp << std::endl;
     strncpy(parent, tmp, last_slash - tmp );
-    std::cout << "parent: " << parent << std::endl;
+    // std::cout << "parent: " << parent << std::endl;
 
     char* current_dir = getenv("PWD");
     char* home = getenv("HOME");
-    std::cout << "home:  " << home << std::endl;
+    // std::cout << "home:  " << home << std::endl;
     const char *delim = "/";
     char * pch;
-    printf ("Splitting string \"%s\" into tokens:\n", my_path);
+    // printf ("Splitting string \"%s\" into tokens:\n", my_path);
     pch = strtok(my_path, delim);
+    std::vector<char*> vec;
     while (pch != NULL)
     {
         if ( strcmp(pch, ".") == 0 )
@@ -110,16 +111,45 @@ bool is_sub_directory(const char* pathname)
         }
         pch = strtok (NULL, delim);
     }
-
     char* absolute_path = new char[200];
     for (auto it = std::begin(vec); it!=std::end(vec); ++it)
     {
         strcat(absolute_path, *it);
     }
-    std::cout << "pwd: " << current_dir << std::endl;
-    std::cout << "abs path: " << absolute_path << std::endl;
+    // std::cout << "pwd: " << current_dir << std::endl;
+    // std::cout << "abs path: " << absolute_path << std::endl;
 
-    return !(strstr(absolute_path, current_dir) == nullptr);
+    char* base_path = getenv("BASE_PATH");
+    char* pch_base;
+    pch_base = strtok(base_path, delim);
+    std::vector<char*> vec_base;
+    while (pch_base != NULL)
+    {
+        if ( strcmp(pch_base, ".") == 0 )
+            vec_base.emplace_back(getenv("PWD"));
+        else if ( strcmp(pch_base, "..") == 0 )
+            vec_base.emplace_back(parent);
+        else if( strcmp(pch_base, "~") == 0 )
+            vec_base.emplace_back(getenv("HOME"));
+        else
+        {
+            char* slash = new char[100];
+            strcpy(slash, "/");
+            strcat(slash, pch_base);
+            vec_base.emplace_back(slash);
+        }
+        pch_base = strtok (NULL, delim);
+    }
+    char* absolute_path_base = new char[200];
+    for (auto it = std::begin(vec); it!=std::end(vec); ++it)
+    {
+        strcat(absolute_path_base, *it);
+    }
+
+    std::cout << "BASE_PATH :" << absolute_path_base << std::endl;
+    std::cout << "ABSOLUTE_PATH :" << absolute_path << std::endl;
+
+    return !(strstr(absolute_path, absolute_path_base) == nullptr);
 }
 
 int chdir(const char* path)
@@ -326,6 +356,7 @@ int unlink(const char *pathname)
 }
 int execl(const char *pathname, const char *arg, .../* (char  *) NULL */)
 {
+    return -82193;
     if ( !is_sub_directory(pathname) )
     {
         std::cerr << "access to " << pathname << " is not allowd." << std::endl;
@@ -349,6 +380,7 @@ int execl(const char *pathname, const char *arg, .../* (char  *) NULL */)
 }
 // int execle(const char *pathname, const char *arg, char *const envp[], .../*, (char *) NULL */)
 // {
+//     return -1;
 //     if ( !is_sub_directory(pathname) )
 //     {
 //     std::cerr << "access to " << pathname << " is not allowd." << std::endl;
@@ -372,6 +404,7 @@ int execl(const char *pathname, const char *arg, .../* (char  *) NULL */)
 // }
 int execlp(const char *file, const char *arg, .../* (char  *) NULL */)
 {
+    return -1;
     if ( !is_sub_directory(file) )
     {
         std::cerr << "access to " << file << " is not allowd." << std::endl;
@@ -386,6 +419,7 @@ int execlp(const char *file, const char *arg, .../* (char  *) NULL */)
 }
 int execv(const char *pathname, char *const argv[])
 {
+    return -1;
     if ( !is_sub_directory(pathname) )
     {
         std::cerr << "access to " << pathname << " is not allowd." << std::endl;
@@ -395,17 +429,17 @@ int execv(const char *pathname, char *const argv[])
     int rtn = old_execv(pathname, argv);
     return rtn;
 }
-int execve(const char *pathname, char *const argv[], char *const envp[])
-{
-    if ( !is_sub_directory(pathname) )
-    {
-        std::cerr << "access to " << pathname << " is not allowd." << std::endl;
-        return -1;
-    }
-    LOAD_LIB(execve);
-    int rtn = old_execve(pathname, argv, envp);
-    return rtn;
-}
+// int execve(const char *pathname, char *const argv[], char *const envp[])
+// {
+//     if ( !is_sub_directory(pathname) )
+//     {
+//         std::cerr << "access to " << pathname << " is not allowd." << std::endl;
+//         return -1;
+//     }
+//     LOAD_LIB(execve);
+//     int rtn = old_execve(pathname, argv, envp);
+//     return rtn;
+// }
 int execvp(const char *file, char *const argv[])
 {
     if ( !is_sub_directory(file) )
